e2a4120fe443292b36c2afddf2c25dba8cf17a74
Nikita Shabulin <nwpress@yandex.ru>
2020-12-06 21:13:04 +0700

    Added new component UserActionIcons

--- src/components/Slider/SliderItem.js
+++ src/components/Slider/SliderItem.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { ReactComponent as IconComplexity } from '../../assets/icons/Complexity.svg';
 import { ReactComponent as IconTimer } from '../../assets/icons/Timer.svg';
 import { ReactComponent as IconCalories } from '../../assets/icons/Calories.svg';
-import { ReactComponent as IconFavWhite } from '../../assets/icons/FavWhite.svg';
-import { ReactComponent as IconPrintWhite } from '../../assets/icons/PrintWhite.svg';
-import { ReactComponent as IconShareWhite } from '../../assets/icons/ShareWhite.svg';
-
-
+import { UserActionIcons } from './UserActionIcons';
 export class SliderItem extends React.Component {
   constructor(props) {
     super(props);
@@ -15,17 +11,7 @@ export class SliderItem extends React.Component {
   render() {
     return(
       <article className='slider_item'>
-        <div className='slider_user-actions'>
-          <div className='slider_user-actions-icon'>
-            <a href='#' className='slider_user-actions-link'><IconFavWhite /></a>
-          </div>
-          <div className='slider_user-actions-icon'>
-            <a href='#' className='slider_user-actions-link'><IconPrintWhite /></a>
-          </div>
-          <div className='slider_user-actions-icon'>
-            <a href='#' className='slider_user-actions-link'><IconShareWhite /></a>
-          </div>
-        </div>
+        <UserActionIcons />
         <h2 className='slider_title'>
           { this.props.slideData.title }
         </h2>
---
+++ src/components/Slider/UserActionIcons.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ReactComponent as IconFavWhite } from '../../assets/icons/FavWhite.svg';
+import { ReactComponent as IconPrintWhite } from '../../assets/icons/PrintWhite.svg';
+import { ReactComponent as IconShareWhite } from '../../assets/icons/ShareWhite.svg';
+
+export class UserActionIcons extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='slider_user-actions'>
+        <div className='slider_user-actions-icon'>
+          <a href='#' className='slider_user-actions-link'>
+            <IconFavWhite className='slider_user-actions-svg'/>
+          </a>
+        </div>
+        <div className='slider_user-actions-icon'>
+          <a href='#' className='slider_user-actions-link'>
+            <IconPrintWhite className='slider_user-actions-svg'/>
+          </a>
+        </div>
+        <div className='slider_user-actions-icon'>
+          <a href='#' className='slider_user-actions-link'>
+            <IconShareWhite className='slider_user-actions-svg'/>
+          </a>
+        </div>
+      </div>
+    );
+  };
+};
