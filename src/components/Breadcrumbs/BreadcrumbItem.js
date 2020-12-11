9a5a30f983587b541e9181f35e47218cf84d2561
Nikita Shabulin <nwpress@yandex.ru>
2020-12-11 13:30:48 +0700

    breadcrumbs link reworked

--- src/components/Breadcrumbs/BreadcrumbItem.js
+++ src/components/Breadcrumbs/BreadcrumbItem.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 export class BreadcrumbsItem extends React.Component {
   render() {
-    return(
+    return (
       <li className='breadcrumbs_item'>
-        <a href={this.props.href} className='breadcrumbs_item-link'>
-          {this.props.text}
-        </a>
+        {this.props.href
+          ? <a href={this.props.href} className='breadcrumbs_item-link'>
+              {this.props.text}
+            </a>
+          : this.props.text
+        }
       </li>
     );
   };
