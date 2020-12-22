8cf32b20d8a49030328f8067ded780fd2c8fdc3d
Nikita Shabulin <nwpress@yandex.ru>
2020-12-22 10:38:07 +0700

    Rework nutritional value box

--- src/components/NutritionalValue/index.js
+++ src/components/NutritionalValue/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { NutritionalValueItem } from './NutritionalValueItem';
+import {NutritionalValueItem} from './NutritionalValueItem';
 
 export class NutritionalValue extends React.Component {
   render() {
-    return(
+    return (
       <div className="nutritional-value_wrap">
         <h2 className="section_heading nutritional-value_heading">
           Пищевая ценность
         </h2>
         <div className='nutritional-value recipe_nutritional-value'>
-        {this.props.data.map(item => {
-          return(
-            <NutritionalValueItem 
-              image={item.image}
-              key={item.id}
-              title={item.title}
-              weight={item.weight}
-              weightUnit={item.weightUnit}
-            />
-          );
-        })}
-      </div>
+          <div className="nutritional-value_values-wrap">
+            {this
+              .props
+              .data
+              .map(item => {
+                return (<NutritionalValueItem
+                  image={item.image}
+                  key={item.id}
+                  title={item.title}
+                  weight={item.weight}
+                  weightUnit={item.weightUnit}/>);
+              })}
+          </div>
+        </div>
       </div>
     );
   };
