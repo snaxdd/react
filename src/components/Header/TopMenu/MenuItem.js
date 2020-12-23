049028d50c01b42eb74bc53308083a387146fd01
Nikita Shabulin <nwpress@yandex.ru>
2020-12-23 23:09:48 +0700

    Top menu reworked

--- src/components/Header/TopMenu/MenuItem.js
+++ src/components/Header/TopMenu/MenuItem.js
@@ -4,11 +4,11 @@ import { DropDown } from './DropDown';
 export class MenuItem extends React.Component {
   render() {
     return (
-      <li className='header_menu-item' data-attr={this.props.dataAttr}>
+      <li className='header_menu-item' data-index={this.props.dataAttrIndex} >
         <a
-          onClick={() => this.props.handleDropdown(this.props.dataAttr)}
           href={this.props.href}
           className='header_menu-link'
+          onClick={() => this.props.handleDropdown(this.props.dataAttrIndex)}
         >
           {this.props.iconL}
           {this.props.text}
@@ -17,7 +17,7 @@ export class MenuItem extends React.Component {
         {this.props.dropDown && (
           <DropDown
             dropDown={this.props.dropDown}
-            isOpen={this.props.isDropDown}
+            isOpen={this.props.activeDropdownId === this.props.dataAttrIndex}
           />
         )}
       </li>
